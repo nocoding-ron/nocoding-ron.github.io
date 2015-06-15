1dd296ab44290383d08e2bb9bc2a16589dcfacc8
Ronnel <ron@24digital.com.au>
2015-06-15 11:27:31 +0800

    video bg

--- js/app.js
+++ js/app.js
@@ -49,22 +49,3 @@ $(document).ready(function(){
     });
 });
 
-function initialize() {
-        var myHouse= new google.maps.LatLng(9.7979036,123.8063313);
-
-        var mapOptions = {
-          center: myHouse,
-          zoom: 12,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          disableDefaultUI: true
-        };
-        var map = new google.maps.Map(document.getElementById('contact-map'),
-            mapOptions);
-
-        var marker = new google.maps.Marker({
-            position:myHouse,
-            map: map,
-        });
-
-}
-      google.maps.event.addDomListener(window, 'load', initialize);
